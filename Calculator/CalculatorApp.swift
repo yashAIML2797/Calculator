22e5ca93b7f4615f6e478697720fe058c8b1836c
Yash Uttekar <yash.uttekar2797@icloud.com>
2024-11-09 11:46:50 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Yash Uttekar on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
